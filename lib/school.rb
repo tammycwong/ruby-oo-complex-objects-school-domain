36cb5dcb31dabd4eba56303cae1ada6014190e00
Tammy <tammycwong@gmail.com>
2021-04-27 15:20:29 -0400

    can't get sort to work

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,33 @@
 # code here!
+class School
+    attr_accessor :roster
+
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+
+    def add_student(student_name, grade)
+        @student_name = student_name
+        @grade = grade 
+
+        if @roster.keys.include?(@grade)
+            @roster[@grade] << @student_name
+        else
+            @roster[@grade] = []
+            @roster[@grade] << student_name
+        end
+    end
+
+    def grade(grade)
+        @grade = grade
+        @roster[@grade]
+    end
+
+    def sort
+        @roster.each do |students|
+            students.sort!
+        end
+    end
+end
+
